25ec17c7aacddb509d856f6befee9f9947895c66
dstefek <70501689+dstefek@users.noreply.github.com>
2024-04-24 17:16:13 -0700

    Update motor.cpp

--- Final Project/src/motor.cpp
+++ Final Project/src/motor.cpp
@@ -1,1 +1,25 @@
+#include <Arduino.h>
+#include <avr/io.h>
+#include "motor.h"
 
+    byte enablePin = 11;
+    byte in1Pin = 10;
+    byte in2Pin = 9;
+    byte potPin = 0;
+
+void initMotor() {  
+    // Pin Setup
+    pinMode(in1Pin, OUTPUT);
+    pinMode(in2Pin, OUTPUT);
+    pinMode(enablePin, OUTPUT);
+}
+
+void setMotor(int speed, bool direction) {
+    analogWrite(enablePin, speed);
+    digitalWrite(in1Pin, !direction);
+    digitalWrite(in2Pin, direction);
+}
+
+int readPotPin() {
+  return potPin;
+}
